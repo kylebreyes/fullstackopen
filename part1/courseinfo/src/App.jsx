3fd756a159fe2c088bd502c22d2b5601019c4491
kaylubr <kylebreyes092005@gmail.com>
2025-05-29 22:16:55 +0800

    Finish 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -8,14 +8,16 @@ const Part = (props) => (
 
 const Content = (props) => (
   <>
-    <Part part={props.p1} exercise={props.e1} />
-    <Part part={props.p2} exercise={props.e2} />
-    <Part part={props.p3} exercise={props.e3} />
+    <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+    <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+    <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
   </>
 )
 
 const Total = (props) => (
-  <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>
+  <p>
+    Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+  </p>
 )
 
 const App = () => {
@@ -38,16 +40,8 @@ const App = () => {
   return (
     <div>
       <Header content={course} />
-      <Content
-        p1={parts[0].name} e1={parts[0].exercises}
-        p2={parts[1].name} e2={parts[1].exercises}
-        p3={parts[2].name} e3={parts[2].exercises}
-      />
-      <Total 
-        e1={parts[0].exercises}
-        e2={parts[1].exercises}
-        e3={parts[2].exercises} 
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
